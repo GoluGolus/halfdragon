61f0a241cf6831bebe56d7ef444c40b99f39fb59
GoluGolus <146285438+GoluGolus@users.noreply.github.com>
2023-09-30 21:29:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,1 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-const token = '';
-
-client.on('ready', () => {
-    console.log(`Connecté en tant que ${client.user.tag}!`);
-});
-
-client.on('messageCreate', (message) => {
-    console.log(`${message.author.tag} a dit : ${message.content}`);
-});
-
-client.login(token);
